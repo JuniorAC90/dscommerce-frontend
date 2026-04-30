07864dde438d50c7bd3296385a09001375e1abe7
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-04-30 08:53:56 -0300

    LoggedUser

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import homeIcon from '../../assets/home.svg';
 import productsIcon from '../../assets/products.svg';
+import LoggedUser from '../LoggedUser';
 
 export default function HeaderAdmin() {
     return (
@@ -8,20 +9,17 @@ export default function HeaderAdmin() {
             <nav className="dsc-container">
                 <h1>DSC Admin</h1>
                 <div className="dsc-navbar-right">
-                <div className="dsc-menu-items-container">
-                    <div className="dsc-menu-item">
-                    <img src={homeIcon} alt="Início" />
-                    <p>Início</p>
+                    <div className="dsc-menu-items-container">
+                        <div className="dsc-menu-item">
+                            <img src={homeIcon} alt="Início" />
+                            <p>Início</p>
+                        </div>
+                        <div className="dsc-menu-item">
+                            <img src={productsIcon} alt="Cadastro de produtos" />
+                            <p className="dsc-menu-item-active">Produtos</p>
+                        </div>
                     </div>
-                    <div className="dsc-menu-item">
-                    <img src={productsIcon} alt="Cadastro de produtos" />
-                    <p className="dsc-menu-item-active">Produtos</p>
-                    </div>
-                </div>
-                <div className="dsc-logged-user">
-                    <p>Maria Silva</p>
-                    <a href="#">Sair</a>
-                </div>
+                    <LoggedUser />
                 </div>
             </nav>
         </header>
