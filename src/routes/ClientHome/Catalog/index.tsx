bdf6b2d317926c6378b1e9fdb98f94e39a51edd5
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-05-01 09:42:10 -0300

    Dynamic product listing

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,7 +5,6 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import * as productService from '../../../services/product-service';
 import { useEffect, useState } from 'react';
 import type { ProductDTO } from '../../../models/product';
-import { hasAnyRoles } from '../../../services/auth-service';
 
 type QueryParams = {
     page: number;
