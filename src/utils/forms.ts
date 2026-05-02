d457641836915e76396bfec9f5c792e33f7cdd9b
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-05-02 15:37:50 -0300

    forms updateAll

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -3,10 +3,18 @@ export function update(inputs: any, name: string, newValue: any) {
 }
 
 export function toValues(inputs: any) {
-
     const data: any = {};
     for (const name in inputs) {
         data[name] = inputs[name].value;
     }
     return data;
 }
+
+export function updateAll(inputs: any, newValues: any) {
+    const newInputs: any = {};
+    for (const name in inputs) {
+        newInputs[name] = {...inputs[name], value: newValues[name]};
+    }
+    return newInputs;
+}
+
